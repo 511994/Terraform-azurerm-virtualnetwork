d4835fb8ff605bf74810adc2c2bc55bb672faf18
suresh <sureshchanti301@gmail.com>
2021-04-27 00:17:21 +0530

    added

---
+++ output.tf
@@ -0,0 +1,9 @@
+output "vnetname" {
+value = azurerm_virtual_network.example.name
+}
+output "rgname" {
+value = azurerm_resource_group.example.name
+}
+
+
+
---
+++ provider.tf
@@ -0,0 +1,7 @@
+provider "azurerm" {
+  features {}
+  subscription_id = "58cac61f-3c9c-4928-9564-5342a4986b3a"
+  client_id       = "e0913fc5-dbfe-47aa-828a-ba82bbd146cf"
+  client_secret   = "8~gMLamZ~r0.Kp2f382_1shn8KA7dXrEt7"
+  tenant_id       = "d7d1eba4-f3c3-4ae1-a8b6-b329cf59fe06"
+}
